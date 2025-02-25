d6f47e435ac06e6918e4ba1d434019e95c196ddf
oyugi <oyugimaurice22@gmail.com>
2025-02-25 14:04:14 +0300

    Map Order Service in API Gateway, and on docker-compose file

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const userRoutes = require("./src/routes/userRoutes");
 const productRoutes = require("./src/routes/productRoutes");
 
@@ -12,7 +14,24 @@ app.use(morgan("dev"));
 // Routes
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
+app.use("/orders", orderRoute);
 
 app.get("/", (req, res) => res.send("API Gateway is Running"));
+
+const services = {
+    user: "http://user-service:5001",
+    product: "http://product-service:5002",
+    order: "http://order-service:5003",
+  };
+  
+  // Middleware to forward requests
+  app.use("/api/users", createProxyMiddleware({ target: services.user, changeOrigin: true }));
+  app.use("/api/products", createProxyMiddleware({ target: services.product, changeOrigin: true }));
+  app.use("/api/orders", createProxyMiddleware({ target: services.order, changeOrigin: true }));
+  
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`API Gateway running on port ${PORT}`));
+
+
 
 module.exports = app;
