8e91dd47ba27a544f6f9cb733a3c7a341108bb13
oyugi <oyugimaurice22@gmail.com>
2025-03-06 15:25:14 +0300

    feat: Add Redis for event-driven notifications(Handle multiple events assynchronously)

--- notification-service/src/services/notificationService.js
+++ notification-service/src/services/notificationService.js
@@ -1,20 +1,16 @@
+
 const notificationRepository = require("../repositories/notificationRepository");
-const { sendEmail, sendSMS, sendWhatsApp } = require("../utils/brevoService");
+const { redisPub } = require("../config/redisConfig");
 
-
+// Publish event instead of directly sending notifications
 const sendNotification = async (userId, type, contact, message) => {
   const notification = await notificationRepository.createNotification({ userId, type, message });
 
-  try {
-    if (type === "email") await sendEmail(contact, "New Notification", message);
-    if (type === "sms") await sendSMS(contact, message);
-    if (type === "whatsapp") await sendWhatsApp(contact, message);
-  } catch (error) {
-    console.error("Notification failed:", error.message);
-  }
+  // Publish notification event to Redis
+  redisPub.publish("notifications", JSON.stringify({ userId, type, contact, message }));
 
   return notification;
-}
+};
 
 const getUserNotifications = async (userId) => {
   return await notificationRepository.getUserNotifications(userId);
@@ -25,3 +21,31 @@ const markNotificationAsRead = async (id) => {
 };
 
 module.exports = { sendNotification, getUserNotifications, markNotificationAsRead };
+
+// const notificationRepository = require("../repositories/notificationRepository");
+// const { sendEmail, sendSMS, sendWhatsApp } = require("../utils/brevoService");
+
+
+// const sendNotification = async (userId, type, contact, message) => {
+//   const notification = await notificationRepository.createNotification({ userId, type, message });
+
+//   try {
+//     if (type === "email") await sendEmail(contact, "New Notification", message);
+//     if (type === "sms") await sendSMS(contact, message);
+//     if (type === "whatsapp") await sendWhatsApp(contact, message);
+//   } catch (error) {
+//     console.error("Notification failed:", error.message);
+//   }
+
+//   return notification;
+// }
+
+// const getUserNotifications = async (userId) => {
+//   return await notificationRepository.getUserNotifications(userId);
+// };
+
+// const markNotificationAsRead = async (id) => {
+//   return await notificationRepository.markAsRead(id);
+// };
+
+// module.exports = { sendNotification, getUserNotifications, markNotificationAsRead };
--- notification-service/src/workers/notificationWorker.js
+++ notification-service/src/workers/notificationWorker.js
@@ -1,29 +1,43 @@
-// const notificationRepository = require("../repositories/notificationRepository");
-// const notificationService = require("../services/notificationService");
+const { redisSub } = require("../config/redisConfig");
+const { sendEmail, sendSMS, sendWhatsApp } = require("../utils/brevoService");
 
-// const notificationWorker = async (job) => {
-//   const notifications = await notificationRepository.getPendingNotifications();
-//   for (const notification of notifications) {
-//     await notificationService.processNotification(notification);
-//   }
+// Subscribe to the "notifications" channel
+redisSub.subscribe("notifications", (err) => {
+  if (err) console.error("Failed to subscribe:", err);
+  else console.log("Subscribed to Redis notifications channel");
+});
+
+// Listen for new events
+redisSub.on("message", async (channel, message) => {
+  if (channel === "notifications") {
+    const { userId, type, contact, message } = JSON.parse(message);
+
+    try {
+      if (type === "email") await sendEmail(contact, "New Notification", message);
+      if (type === "sms") await sendSMS(contact, message);
+      if (type === "whatsapp") await sendWhatsApp(contact, message);
+      console.log(`Notification sent via ${type} to ${contact}`);
+    } catch (error) {
+      console.error("Error sending notification:", error.message);
+    }
+  }
+});
+
+
+// const sendEmail = require("../utils/sendEmail");
+// const sendSMS = require("../utils/sendSMS");
+// const sendWhatsApp = require("../utils/sendWhatsApp");
+
+// const processEmail = async (job) => {
+//   await sendEmail(job.data.userId, job.data.message);
 // };
 
-// module.exports = notificationWorker;
+// const processSMS = async (job) => {
+//   await sendSMS(job.data.userId, job.data.message);
+// };
 
-const sendEmail = require("../utils/sendEmail");
-const sendSMS = require("../utils/sendSMS");
-const sendWhatsApp = require("../utils/sendWhatsApp");
+// const processWhatsApp = async (job) => {
+//   await sendWhatsApp(job.data.userId, job.data.message);
+// };
 
-const processEmail = async (job) => {
-  await sendEmail(job.data.userId, job.data.message);
-};
-
-const processSMS = async (job) => {
-  await sendSMS(job.data.userId, job.data.message);
-};
-
-const processWhatsApp = async (job) => {
-  await sendWhatsApp(job.data.userId, job.data.message);
-};
-
-module.exports = { processEmail, processSMS, processWhatsApp };
+// module.exports = { processEmail, processSMS, processWhatsApp };
