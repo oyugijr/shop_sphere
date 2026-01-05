3ded94249a0286afd7a46b6c14d0e8478b5f106f
Oyugi <oyugimaurice22@gmail.com>
2026-01-05 23:52:47 +0300

    feat(notification): harden http entrypoint

--- notification-service/src/config/brevoConfig.js
+++ notification-service/src/config/brevoConfig.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const axios = require("axios");
 
 // Bravo API Base URL & Key
-const BREVO_API_URL = process.env.BREVO_API_URL;
+const BREVO_API_URL = process.env.BREVO_API_URL ||  "https://api.brevo.com/v3";
 const BREVO_API_KEY = process.env.BREVO_API_KEY;
 
 // Axios instance for API requests
